bf95120493140d2cfada34b196c20f50c2c04dc6
= <=>
2017-12-10 16:21:58 +0800

    rrr

--- CustomWindow/AppDelegate/AppDelegate.m
+++ CustomWindow/AppDelegate/AppDelegate.m
@@ -14,12 +14,10 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(AppDelegate *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     return YES;
 }
-
 
 - (void)applicationWillResignActive:(AppDelegate *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -32,11 +30,9 @@
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
-
 - (void)applicationWillEnterForeground:(AppDelegate *)application {
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
 }
-
 
 - (void)applicationDidBecomeActive:(AppDelegate *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
