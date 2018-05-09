88ecceb49807ac03ea019486e576baa8b3ea83da
= <=>
2018-05-10 01:24:10 +0800

    添加追

---
+++ CustomWindow/EfficientCornerRadius/textIgn.h
@@ -0,0 +1,27 @@
+//
+//  textIgn.h
+//  CustomWindow
+//
+//  Created by Stephanie on 2018/5/10.
+//  Copyright © 2018年 Stephanie. All rights reserved.
+//
+
+#ifndef textIgn_h
+#define textIgn_h
+
+/*fsdf
+ sfdsfsdfd\sdfsdafsdafsdf
+ hiuyhiuyiuyiysdfsadfsdfsdafsdafsdaf
+ sdfasdfsdafsadsafsasadfasdf
+ sadfsdafsadsretsrdgdrsgdfgdsfgsdfgsdfg
+ fsdkafjksdljfkdsjfjsdkfjkltewjrejr
+ fsdkfjklsdfsdfsdaf
+ sdf
+ sadsdfasdfasdfsdf
+ ffsdsafd
+ sadfsadfsdafsadfsdafsdfsda
+ sdafasdfasdfasdfsadfsdfs
+ sadfaserewarewrfwewrwwer
+ */
+sdfsadfsad 
+#endif /* textIgn_h */
